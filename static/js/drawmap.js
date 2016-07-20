a84bfc0339f777706118455d53098180c98967b3
Joshua Hull <jhull@ccihealth.org>
2016-07-20 13:57:55 -0400

    Map Fixes

--- static/js/drawmap.js
+++ static/js/drawmap.js
@@ -1,5 +1,5 @@
 function drawMap() {
-    var map = L.map('map').setView([34.851354, -82.3984881], 12);
+    var map = L.map('mapbox').setView([34.851354, -82.3984881], 12);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
